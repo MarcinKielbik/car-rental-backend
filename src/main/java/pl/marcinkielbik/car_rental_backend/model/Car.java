19b15b4201dd206e028fc5d779fc230c2e0ab2ba
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-09-24 20:59:20 +0200

    Add Car and Rental model classes with JPA annotations

--- src/main/java/pl/marcinkielbik/car_rental_backend/model/Car.java
+++ src/main/java/pl/marcinkielbik/car_rental_backend/model/Car.java
@@ -1,4 +1,22 @@
 package pl.marcinkielbik.car_rental_backend.model;
 
+import jakarta.persistence.*;
+
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String brand;
+    private String model;
+    private Double pricePerDay;
+
+    @Enumerated(EnumType.STRING)
+    private CarStatus status;
+
+    public enum CarStatus {
+        AVAILABLE,
+        RENTED
+    }
+
 }
--- src/main/java/pl/marcinkielbik/car_rental_backend/model/Rental.java
+++ src/main/java/pl/marcinkielbik/car_rental_backend/model/Rental.java
@@ -1,4 +1,32 @@
 package pl.marcinkielbik.car_rental_backend.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
 public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", nullable = false)
+    private Car car;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    private RentalStatus status;
+
+
+    public enum RentalStatus {
+        ACTIVE,
+        RETURNED
+    }
 }
